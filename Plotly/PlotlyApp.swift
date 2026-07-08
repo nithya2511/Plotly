ab5fd56fce1a097277e22c85361ac1a3678116fd
Nithya Vasudevan <nithya1vasudevan@gmail.com>
2026-07-08 22:50:15 +0200

    Initial Commit

---
+++ Plotly/PlotlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlotlyApp.swift
+//  Plotly
+//
+//  Created by Nithya Vasudevan on 08.07.26.
+//
+
+import SwiftUI
+
+@main
+struct PlotlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
